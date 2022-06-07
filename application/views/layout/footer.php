4414b7b1f259159984050304c0b082e19738da1d
Edoleonardo <edoleonardo198@gmail.com>
2022-06-07 16:06:27 +0700

    fix import library

--- application/views/layout/footer.php
+++ application/views/layout/footer.php
@@ -13,6 +13,19 @@
 <!-- /.control-sidebar -->
 </div>
 <!-- ./wrapper -->
+<!-- jQuery -->
+<script src="<?php echo base_url(); ?>assets/plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+<script src="<?php echo base_url(); ?>assets/dist/js/adminlte.js"></script>
+<!-- <script src="<?php echo base_url(); ?>assets/plugins/chart.js/Chart.min.js"></script> -->
+<!-- <script src="<?php echo base_url(); ?>assets/dist/js/pages/dashboard3.js"></script> -->
+<script src="<?php echo base_url(); ?>assets/dist/js/demo.js"></script>
+<script src="<?php echo base_url(); ?>assets/plugins/jszip/jszip.min.js"></script>
+<script src="<?php echo base_url(); ?>assets/plugins/pdfmake/pdfmake.min.js"></script>
+<script src="<?php echo base_url(); ?>assets/plugins/pdfmake/vfs_fonts.js"></script>
+<script src="<?php echo base_url(); ?>assets/plugins/sweetalert2/sweetalert2.min.js"></script>
+<script src="<?php echo base_url(); ?>assets/plugins/toastr/toastr.min.js"></script>
+<script src="<?php echo base_url(); ?>assets/plugins/bs-custom-file-input/bs-custom-file-input.min.js"></script>
+<script src="//cdn.jsdelivr.net/npm/sweetalert2@11"></script>
 </body>
 
 </html>
