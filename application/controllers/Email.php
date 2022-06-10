e74239f82522a42b02aa8cc8e5b69d2a5d670af3
Edoleonardo <edoleonardo198@gmail.com>
2022-06-10 09:31:10 +0700

    fix email

--- application/controllers/Email.php
+++ application/controllers/Email.php
@@ -43,47 +43,50 @@ class Email extends CI_Controller
                 $mail->FromName = "Edooo";          //Sets //Adds a "From" name
                 $mail->WordWrap = 5000;              //Sets word wrapping on the body of the message to a given number of characters
                 $mail->IsHTML(true);              //Sets message type to HTML
-                $mail->addAttachment($_FILES['attachment']['tmp_name'],$_FILES['attachment']['name']);
+                $mail->addAttachment($_FILES['attachment']['tmp_name'], $_FILES['attachment']['name']);
                 $mail->Body = $this->input->post('body') ? $this->input->post('body') : " ";
                 $mail->Subject = $this->input->post('subject') ? $this->input->post('subject') : "No Subject";
 
-                
+
                 // $tmp_to = $this->pic_model->get_all_active_pic_email();
-                $tmp_cc = $this->pic_model->get_all_active_pic_email2();
-                // $tmp_bcc = $this->pic_model->get_all_active_pic_email3();
                 // foreach ($tmp_to as $data) {
                 //     $mail->AddAddress($data->email, $data->name);
                 // }
-                $mail->AddAddress('rgsetiawan@rintis.co.id', 'asd');
-                $length = count((array)$tmp_cc);
-                $temp = ceil($length / 20);
-                // var_dump($temp);
-                // die;
-                // for($i = 0; $i < $temp;$i++){
+
+                // $tmp_cc = $this->pic_model->get_all_active_pic_email2();
+                // foreach ($tmp_cc as $data) {
+                //     $mail->AddCC($data->email, $data->name);
                 // }
-                    $i = 0;
-                    $c = 1;
-                foreach ($tmp_cc as $data) {
-                    $mail->AddCC($data->email, $data->name);
-                    if($i == 20){
+
+                $tmp_bcc = $this->pic_model->get_all_active_pic_email3();
+                // foreach ($tmp_bcc as $data) {
+                //     $mail->addBCC($data->email, $data->name);
+                // }
+
+                $mail->AddAddress('rgsetiawan@rintis.co.id', 'asd');
+
+                $length = count((array)$tmp_bcc);
+                $temp = ceil($length / 20);
+
+                $i = 0; // flag email
+                $c = 1; // flag send
+                foreach ($tmp_bcc as $data) {
+                    $mail->addBCC($data->email, $data->name);
+                    if ($i == 20) {
                         $mail->send();
-                        $mail->clearCCs();
+                        $mail->clearBCCs();
                         $c++;
                         $i = 0;
                     }
-                    if($c == $temp){
+                    if ($c == $temp) {
                         $mail->send();
                     }
                     $i++;
                 }
-                // foreach ($tmp_bcc as $data) {
-                //     $mail->addBCC($data->email, $data->name);
-                // }
-                
 
-                $msg = ['sukses'=>"Email Berhasil Terkirim"];
+                $msg = ['sukses' => "Email Berhasil Terkirim"];
             } catch (Exception $e) {
-                $msg = ['error'=>"Message could not be sent. Mailer Error: {$mail->ErrorInfo}"];
+                $msg = ['error' => "Message could not be sent. Mailer Error: {$mail->ErrorInfo}"];
             }
             echo json_encode($msg);
         }
