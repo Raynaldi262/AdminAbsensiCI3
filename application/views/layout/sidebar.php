90a5f3c8339185b4dfd568e42ea08d71b8b8f352
raynaldi262 <ginantaraaldi1387@gmail.co.id>
2024-05-24 12:53:14 +0700

    asd

--- application/views/layout/sidebar.php
+++ application/views/layout/sidebar.php
@@ -32,7 +32,7 @@
 <aside class="main-sidebar sidebar-dark-primary elevation-4">
   <!-- Brand Logo -->
   <a href="#" class="brand-link">
-    <img src="<?php echo base_url() ?>/assets/dist/img/honda2.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
+    <img src="<?php echo base_url() ?>/assets/dist/img/Honda2.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
     <span class="brand-text font-weight-light">Admin</span>
   </a>
 
